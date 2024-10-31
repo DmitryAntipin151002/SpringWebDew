eaafd30ff6ae4a20e6ef0a1f675decf78cbe8db1
DmitryAntipin <dmitry1986antipin@gmail.com>
2024-10-31 09:35:39 +0300

    New project

---
+++ SpringWebDew/src/main/java/com/example/SpringWebDew/SpringWebDewApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebDew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDewApplication.class, args);
+	}
+
+}
